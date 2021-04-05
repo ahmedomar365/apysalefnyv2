a8cb94fb17c8cab4984968e960400d002144be68
ahmedomar365 <70892817+ahmedomar365@users.noreply.github.com>
2021-04-05 21:31:16 +0200

    Add user and index routes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,55 @@ router.get('/', (req, res, next) => {
   res.render('index', { title: 'Surf Shop - Home' });
 });
 
+/* GET /register. */
+router.get('/register', (req, res, next) => {
+  res.send("Get /register");
+});
+
+/* POST /register. */
+//this is for creating the user.
+router.post('/register', (req, res, next) => {
+  res.send("POST /register");
+});
+
+/* get /login. */
+router.get('/login', (req, res, next) => {
+  res.send("get /login");
+});
+
+/* POST /login. */
+router.post('/login', (req, res, next) => {
+  res.send("post /login");
+});
+
+/* GET /profile */
+router.get('/profile', (req, res, next) => {
+  res.send("GET /profile");
+});
+
+//so that you can update the user info which you got from the login
+//so the profile will get this data and then you will be able to update this information
+/* PUT /profile/:user_id */
+router.put('/profile/:user_id', (req, res, next) => {
+  res.send("PUT /profile/:user_id");
+});
+
+/* GET /forgot */
+router.get('/forgot', (req, res, next) => {
+  res.send("GET /forgot");
+});
+
+/* PUT /forgot */
+router.put('/forgot', (req, res, next) => {
+  res.send("PUT /forgot");
+});
+
+/* GET /reset */
+router.get('/reset/:token', (req, res, next) => {
+  res.send("GET /reset/:token");
+});
+/* PUT /reset */
+router.put('/reset/:token', (req, res, next) => {
+  res.send("PUt /reset/:token");
+});
 module.exports = router;
