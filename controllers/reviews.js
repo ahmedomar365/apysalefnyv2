370a2c7d8fc022e10277080d8bb64e743efec8dc
ahmedomar365 <70892817+ahmedomar365@users.noreply.github.com>
2021-04-19 07:19:07 +0200

    tda

---
+++ controllers/reviews.js
@@ -0,0 +1,17 @@
+const Post = require('../models/post');
+const Review = require('../models/review');
+
+module.exports = {
+    //Reviews Create
+    async reviewCreate(req, res, next) {
+      
+    },
+    // Reviews Update
+    async reviewUpdate(req, res, next) {
+
+    },
+    // Reviews destory
+    async reviewDestroy(req, res, next) {
+
+    }
+}
--- models/review.js
+++ models/review.js
@@ -3,6 +3,7 @@ const Schema = mongose.Schema;
 
 const ReviewShema = new Schema({
     body: String,
+    rating: Number,
     author: {
         type: Schema.Types.ObjectId,
         ref: 'User'
